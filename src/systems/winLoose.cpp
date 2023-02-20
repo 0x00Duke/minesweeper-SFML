4da29ace8644c848e5f45e9259cc32e30c3c4b28
0x00Duke <leon.ducasse@epitech.eu>
2023-02-20 13:12:25 +0100

    [ADD] check if win

--- src/systems/winLoose.cpp
+++ src/systems/winLoose.cpp
@@ -24,11 +24,18 @@ void WinLooseSystem::CheckLoose(sf::RenderWindow *window)
 
 void WinLooseSystem::CheckWin(sf::RenderWindow *window)
 {
+    for (auto const &entity : mEntities) {
+        auto &tile = gCoordinator.GetComponent<Tile>(entity);
+        if (tile.sValue != tile.value && tile.sValue != 11)
+            return;
+    }
+    sf::sleep(sf::seconds(1));
     std::cout << "You win !" << std::endl;
+    window->close();
 }
 
 void WinLooseSystem::update(sf::Event event, sf::RenderWindow *window)
 {
     CheckLoose(window);
-    // CheckWin(window);
+    CheckWin(window);
 }
